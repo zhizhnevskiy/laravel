15f3e94e2cc809ea7b1e9b443a1d53e9ed013eb7
y.zhizhnevskiy <CN7FFmHS>
2022-09-30 17:33:10 +0300

    added logs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\Rubric;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,9 +27,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-//        DB::listen(function ($query){
+        DB::listen(function ($query){
 //            dump($query->sql);
-//        });
+//            Log::info($query->sql);
+            Log::channel('sqllogs')->info($query->sql);
+        });
 
         view()->composer('layouts.footer', function($view){
             $view->with('rubrics', Rubric::all());
--- config/logging.php
+++ config/logging.php
@@ -60,6 +60,12 @@ return [
             'level' => env('LOG_LEVEL', 'debug'),
         ],
 
+        'sqllogs' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/sql.log'),
+            'level' => env('LOG_LEVEL', 'debug'),
+        ],
+
         'daily' => [
             'driver' => 'daily',
             'path' => storage_path('logs/laravel.log'),
